9ac798a2d32f90753d1ad14529c05557a9c51e5a
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-11 00:38:21 +0300

    added to book.py

--- lib/book.py
+++ lib/book.py
@@ -2,5 +2,25 @@
 
 class Book:
     pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+    
+    @property
+    def page_count(self):
+        """Get the page count."""
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        """Set the page count with validation."""
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+    
+    def turn_page(self):
+        """Simulate turning a page."""
+        print("Flipping the page...wow, you read fast!")
     
         
