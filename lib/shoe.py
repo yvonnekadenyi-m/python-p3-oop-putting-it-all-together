9617d7dcd56f386692e9e207b08965f381fb8bdb
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-11 00:39:09 +0300

    added to shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,35 @@
+
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        """Get the shoe size."""
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        """Set the shoe size with validation."""
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        """Simulate repairing the shoe."""
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    
+    @property
+    def condition(self):
+        """Get the condition of the shoe."""
+        return self._condition if hasattr(self, '_condition') else None
+    
+    @condition.setter
+    def condition(self, value):
+        """Set the condition of the shoe."""
+        self._condition = value
